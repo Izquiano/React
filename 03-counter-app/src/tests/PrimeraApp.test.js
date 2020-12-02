ef14c4e61a02dd076e1d087918b4161a93d370b7
Izquiano <izquiano@hotmail.com>
2020-12-02 12:49:23 +0100

    Configuarción de enzyme para test en React

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react'
 import PrimeraApp from '../PrimeraApp';
+import { shallow } from 'enzyme';
 
 
 describe('Pruebas en <PrimeraApp.js />', () => {
-  test('debe mostrar el mensaje "Hola, soy Goku"', () => {
-    const saludo = 'Hola, soy Goku'
-    const wrapper = render( <PrimeraApp saludo={saludo}/>)
+  // test('debe mostrar el mensaje "Hola, soy Goku"', () => {
+  //   const saludo = 'Hola, soy Goku'
+  //   const wrapper = render( <PrimeraApp saludo={saludo}/>)
+  //   expect( wrapper.getByText(saludo) ).toBeInTheDocument()
+  // })
 
-    expect( wrapper.getByText(saludo) ).toBeInTheDocument()
+  test('Debe mostrar <PrimeraApp /> correctamente', () => {
+    const saludo = 'Hola, soy Goku'
+    const wrapper = shallow( <PrimeraApp saludo={ saludo }/>)
+    expect( wrapper ).toMatchSnapshot()
   })
+  
   
 })
