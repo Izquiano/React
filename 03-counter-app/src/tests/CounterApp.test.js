608a88333b2a8294a7a7a364a965b547bfd59197
Izquiano <izquiano@hotmail.com>
2020-12-03 13:28:53 +0100

    TEST con beforeEach para inicializar la variable que buscamos en cada test dentro del expect

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -1,23 +1,40 @@
-const { shallow } = require("enzyme")
-const { default: CounterApp } = require("../CounterApp")
+const { shallow } = require("enzyme");
+const { default: CounterApp } = require("../CounterApp");
 
-describe('Pruebas en el <CounterApp />', () => {
+describe("Pruebas en el <CounterApp />", () => {
+  let wrapper = shallow(<CounterApp />);
   
+  beforeEach(() => {
 
-  test('debe mostrar el <CounterApp /> correctamente', () => {
-    
-    const wrapper = shallow( <CounterApp />)
-    expect( wrapper ).toMatchSnapshot() 
-    
+    wrapper = shallow(<CounterApp />);
     
   })
-  
-  test('debe mostrar el valor por defecto de 100', () => {
-    const wrapper = shallow( <CounterApp value={100}/>)
-    const counterText = wrapper.find('h2').text().trim()
-    console.log(counterText)
-    expect( counterText ).toBe('100')
-  })
-  
-  
-})
+
+  test("debe mostrar el <CounterApp /> correctamente", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("debe mostrar el valor por defecto de 100", () => {
+    const wrapper = shallow(<CounterApp value={100} />);
+    const counterText = wrapper.find("h2").text().trim();
+    console.log(counterText);
+    expect(counterText).toBe("100");
+  });
+
+  test("Debe incrementar con el botón +1", () => {
+    const btn1 = wrapper.find("button").at(0).simulate("click");
+    // console.log(btn1.html())
+    const counterText = wrapper.find("h2").text().trim();
+    expect(counterText).toBe("11");
+  });
+
+  test("Debe restar 1 con el botón -1", () => {
+    const btn1 = wrapper.find("button").at(2).simulate("click");
+    // console.log(btn1.html())
+    const counterText = wrapper.find("h2").text().trim();
+    expect(counterText).toBe("9");
+  });
+
+
+
+});
