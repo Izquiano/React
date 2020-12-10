09be89ed0532c97e35da9d0936a6f9da81c0e8ef
Izquiano <izquiano@hotmail.com>
2020-12-10 18:11:06 +0100

    Test para props requeridas en un componente (el snapshot se actualiza con la tecla u)

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -4,8 +4,12 @@ import { GifGridItem } from '../../components/GifGridItem'
 
 
 describe('Pruebas en <GifGridItem />', () => {
+
+  const title = 'Un título'
+  const url = 'https://localhost/algo.jpg'
+
   test('Debe mostrar el componente correctamente', () => {
-    const wrapper = shallow ( <GifGridItem />)
+    const wrapper = shallow ( <GifGridItem title={ title } url={ url }/>)
     expect( wrapper ).toMatchSnapshot()
     
   })
