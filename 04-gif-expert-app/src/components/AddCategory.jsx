efa7c50dc6c6e13dab77e3c7db665ffb5e30075f
Izquiano <izquiano@hotmail.com>
2020-12-11 19:32:09 +0100

    Simulando el submit en el formulario de AddCategory

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const AddCategory = ({ setCategories }) => {
+export const AddCategory = ({ setCategories }) => {
 
 
   const [inputValue, setInputValue] = useState('')
 
   const handleInputChange = (e) => {
     setInputValue( e.target.value ) 
+    // console.log('handleInputChange llamado')
   }
 
   const handleSubmit = (e) => {
@@ -17,11 +18,13 @@ const AddCategory = ({ setCategories }) => {
       setCategories( prev => [ inputValue, ...prev ] )
       setInputValue('')
     }
+    console.log('handleSubmit', inputValue)
 
   }
 
   return (
     <form onSubmit={ handleSubmit }>
+      <p>{ inputValue }</p>
       <input 
         type="text"
         value={ inputValue }
@@ -36,4 +39,4 @@ const AddCategory = ({ setCategories }) => {
  AddCategory.propTypes = {
    setCategories: PropTypes.func.isRequired
  }
-export default AddCategory
+
