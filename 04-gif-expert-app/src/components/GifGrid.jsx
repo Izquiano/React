98f4c33aadc5321d676eb155e7f538fa7cb9d315
Izquiano <izquiano@hotmail.com>
2020-12-11 18:10:25 +0100

    Test para que un div reciba una className concreta

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,42 +1,23 @@
-import React from 'react'
-import { useFechGifs } from '../hooks/useFetchGifs'
+import React from "react";
+import { useFechGifs } from "../hooks/useFetchGifs";
 
-import { GifGridItem } from './GifGridItem'
+import { GifGridItem } from "./GifGridItem";
 
-export const GifGrid = ( {category} ) => {
-
-  
-
-  const { data: images, loading } = useFechGifs( category )
-
-  // useEffect( ()=> {
-  //   getGifs( category )
-  //   .then( imgs => setImages( imgs ))
-  // }, [ category ] )
-
-  
+export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFechGifs(category);
 
   return (
     <>
-    <h3 className="animate__animated animate__fadeIn">{ category }</h3>
-    {loading && <p className="animate__animated animate__flash">Loading...</p> }
-  
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
+      {loading && (
+        <p className="animate__animated animate__flash">Loading...</p>
+      )}
 
-    <div className="card-grid">
-           
-       {
-         images.map( el => {
-           return (
-            <GifGridItem 
-              key={ el.id } 
-              { ...el }
-            />
-           )
-         })
-       }
-       
-    
-    </div>
+      <div className="card-grid">
+        {images.map((el) => {
+          return <GifGridItem key={el.id} {...el} />;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
