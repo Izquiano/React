245ab447cf873527916b0a7fc69aaccb688c45ef
Izquiano <izquiano@hotmail.com>
2020-12-28 17:31:51 +0100

    useEffect precauciones

--- 05-hook-app/src/components/02-useEffect/Message.jsx
+++ 05-hook-app/src/components/02-useEffect/Message.jsx
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('componente Montado')
+    const mouseMove = (e) => {
+      // console.log(e)
+      const coords = {
+        x: e.x,
+        y: e.y,
+      };
+      setCoords(coords);
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
     return () => {
-      console.log('componente Desmontado')
-      
-    }
-  }, [])
-
-
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
 
   return (
     <div>
       <h3>Eres Genial</h3>
+      <p>
+        x: {x}
+        <br />
+        y: {y}
+      </p>
     </div>
-  )
-}
+  );
+};
